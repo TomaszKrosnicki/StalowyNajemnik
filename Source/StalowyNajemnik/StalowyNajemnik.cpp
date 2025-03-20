e8fdc6a8b6cdfc6c45303aa360c6c9e838b27e18
TomaszKrosnicki <krosnickitomek@gmail.com>
2025-03-20 02:56:59 +0100

    1. Initial commit

---
+++ Source/StalowyNajemnik/StalowyNajemnik.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StalowyNajemnik.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StalowyNajemnik, "StalowyNajemnik" );
